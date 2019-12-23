1b7afa77420ed3aa3c623ce334b29bbc9e7866eb
Erikão Bolado <erik.antunes@live.com>
2019-12-23 14:27:55 -0300

    first commit

---
+++ app/src/main/java/com/titanz/mservices/Splash1.java
@@ -0,0 +1,29 @@
+package com.titanz.mservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash1);
+        irParaSplash2();
+    }
+
+    private void irParaSplash2() {
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), Splash2.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4500);
+    }
+}
---
+++ app/src/main/java/com/titanz/mservices/Splash2.java
@@ -0,0 +1,30 @@
+package com.titanz.mservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash2);
+        irParaMain();
+    }
+
+    private void irParaMain() {
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3500);
+    }
+}
+
